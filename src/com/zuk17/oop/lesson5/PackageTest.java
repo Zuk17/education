ad34edbad320c0cedd19b982dd977f0999ac6d48
Zuk17 <ivan.p.zhukov@gmail.com>
2022-03-28 17:42:52 +0300

    finished AmazingNumber

--- src/com/zuk17/oop/lesson5/PackageTest.java
+++
@@ -1,9 +0,0 @@
-package com.zuk17.oop.lesson5;
-
-import com.zuk17.oop.lesson2.Computer;
-
-public class PackageTest {
-    public static void main(String[] args) {
-        Computer computer = new Computer();
-    }
-}
